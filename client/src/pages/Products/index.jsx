b6f693af227d7015d107efcaf6dad89e17127706
Babashov Mahammad <babashov.info@gmail.com>
2023-01-30 16:42:23 +0400

    added Load More with useInfiniteQuery in react-query

--- client/src/pages/Products/index.jsx
+++ client/src/pages/Products/index.jsx
@@ -1,22 +1,69 @@
-import { useQuery } from 'react-query'
-import { Grid } from '@chakra-ui/react'
+import { useInfiniteQuery } from 'react-query'
+import { Grid,Button,Box,Flex } from '@chakra-ui/react'
 import Card from '../../component/Card'
 import React from 'react'
 import { fetchProductsList } from '../../api'
 
 function Products() {
-  const { isLoading, error, data } = useQuery('products', fetchProductsList)
+  const { 
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status, 
+  } =   useInfiniteQuery('products', fetchProductsList,{
+      
+    getNextPageParam:(lastPage, pages)=> {
+      const morePageExist = lastPage?.length === 12;
+      
+      if(!morePageExist) return;
+
+     return pages.length +1;
+
+    },
+
+  })
  
-   if (isLoading) return 'Loading...'
+   if (status === 'loading') return 'Loading...'
  
-   if (error) return 'An error has occurred: ' + error.message
-   console.log("data",data)
+   if (status === 'error') return 'An error has occurred: ' + error.message
+
   return (
     <div>
 
-        <Grid templateColumns='repeat(3, 1fr)' gap={4}>
+        {/* <Grid templateColumns='repeat(3, 1fr)' gap={4}>
           {data.map((item,index)=><Card key={index} item={item} />)}
-        </Grid>
+        </Grid> */}
+        <Grid templateColumns='repeat(3, 1fr)' gap={4}>
+      {data.pages.map((group, i) => (
+        <React.Fragment key={i}>
+          
+          {group.map((item) => (
+            <Box width="100%" key={item._id}>
+              <Card item={item} />
+            </Box>
+          ))}
+          
+        </React.Fragment>
+      ))}
+      
+      </Grid>
+
+      <Flex mt="10" justifyContent="center">
+        <Button display="flex" flexDirection="center"
+          onClick={() => fetchNextPage()}
+          isLoading={isFetching ? true : false}
+          isDisabled={!hasNextPage || isFetchingNextPage}
+        >
+          {isFetchingNextPage
+            ? 'Loading more...'
+            : hasNextPage
+            ? 'Load More'
+            : 'Nothing more to load'}
+        </Button>
+      </Flex>
 
     </div>
   )
