d5dace8c3274917a10fc8d830f98b712d12bdce7
Babashov Mahammad <babashov.info@gmail.com>
2023-01-31 17:42:18 +0400

    added to basket in products list page is ready

--- client/src/component/Card/index.jsx
+++ client/src/component/Card/index.jsx
@@ -1,9 +1,11 @@
 import {Box,Image,Button} from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 import moment from 'moment';
+import {useBasket} from '../../contexts/BasketContext'
 
 function Card({item}) {
-
+const {items,addToBasket} = useBasket()
+const findBasketItem = items.find((basketItem)=>basketItem._id === item._id)
   return (
     <Box borderWidth='1px' borderRadius='lg' overflow='hidden' p='3'>
         <Link to={`/product/${item._id}`}>
@@ -18,7 +20,12 @@ function Card({item}) {
                 <Box>{item.price} AZN</Box>
             </Box>
         </Link>
-        <Button colorScheme="pink">Add to Basket</Button>
+        <Button 
+            onClick={()=>addToBasket(item,findBasketItem)}
+            colorScheme={findBasketItem ? 'pink' : 'green'}
+        >
+            {findBasketItem ? 'Remove From Basket' : 'Add to Basket'}
+        </Button>
     </Box>
   )
 }
