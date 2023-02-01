078055b19dc22ae9a80853a0399fdcb192df6547
Babashov Mahammad <babashov.info@gmail.com>
2023-02-01 17:36:54 +0400

    admin/products page & antd table & delete button is ready

--- client/src/api.js
+++ client/src/api.js
@@ -61,3 +61,8 @@ export const fetchOrders = async()=>{
     const {data} = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/order`)
     return data;
 }
+
+export const deleteProduct = async (productId)=>{
+    const {data} = await axios.delete(`${process.env.REACT_APP_BASE_ENDPOINT}/product/${productId}`)
+    return data;
+}
--- client/src/pages/Admin/Products/index.jsx
+++ client/src/pages/Admin/Products/index.jsx
@@ -1,8 +1,82 @@
-import React from 'react'
+import { useMemo } from 'react'
+import {useQuery,useMutation,useQueryClient} from 'react-query'
+import {fetchProductsList,deleteProduct} from '../../../api'
+import {Table,Popconfirm} from 'antd'
+import {Text} from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 
 function Products() {
+  const queryClient = useQueryClient()
+  const {isLoading,isError,error,data} = useQuery('admin:products',fetchProductsList)
+
+  const deleteMutation = useMutation(deleteProduct,{
+    onSuccess:()=>queryClient.invalidateQueries('admin:products')
+  })
+
+  const columns = useMemo(()=>{
+    return [
+      {
+        title:'Title',
+        dataIndex:'title',
+        key:'title'
+      },
+      {
+        title:'Price',
+        dataIndex:'price',
+        key:'price'
+      },
+      {
+        title:'Created At',
+        dataIndex:'createdAt',
+        key:'createdAt'
+      },
+      {
+        title:'Action',
+        key:'action',
+        render:(text,record)=>(
+          <>
+          
+            <Link to={`/admin/products/${record._id}`}>Edit</Link>
+  
+            <Popconfirm
+              title='Are you sure'
+              onConfirm={()=>{
+                deleteMutation.mutate(record._id,{
+                  onSuccess:()=>{
+                    console.log('Successfully deleted');
+                  }
+                })
+              }}
+              onCancel={()=>console.log('Canceled')}
+              okText='Beli'
+              cancelText='Xeyr'
+              placement='right'
+            >
+              <a href='/#' style={{marginLeft:10}}>Delete</a>
+            </Popconfirm>
+  
+          </>
+        )
+      }
+    ]
+  },[])
+
+  if(isLoading)
+  {
+    return <div>Loading...</div>
+  }
+
+  if(isError)
+  {
+    return <div>Error: {error.message}</div>
+  }
+
   return (
-    <div>Products</div>
+    <div>
+      <Text fontSize="2xl" p='5'>Products</Text>
+
+      <Table dataSource={data} columns={columns} rowKey="_id"/>
+    </div>
   )
 }
 
